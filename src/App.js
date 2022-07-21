69fc7aa1dc0075369deba72685a52649bb0db3d2
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-07-21 15:02:11 -0500

    :globe_with_meridians: rutas add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { Container } from '@chakra-ui/react'
 
-import Auth from './components/Auth'
+// import Auth from './components/Auth'
+import AppRouter from './routers';
 
 function App() {
   return (
     <div className="App">
       <Container maxW='100%' h='100vh'>
-        <Auth/>
+        <AppRouter />
       </Container>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { BrowserRouter } from 'react-router-dom'
+
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react'
 
@@ -10,7 +12,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ChakraProvider>
 );
 
